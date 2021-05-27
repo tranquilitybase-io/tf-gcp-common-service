b072d86ac4586261a1fbd8352feee77b6d137f9e
e-amce <adam.crow@gft.com>
2021-05-27 19:55:14 +0100

    inital commit

--- examples/simple/outputs.tf
+++ examples/simple/outputs.tf
@@ -1,3 +1,3 @@
-output "random_password" {
-  value = module.simple_secret_manager.random_password
+output "random_passwords" {
+  value = module.simple_secret_manager.random_passwords
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "random_password" {
+output "random_passwords" {
   value = random_password.password.*.result
   sensitive = true
 }
